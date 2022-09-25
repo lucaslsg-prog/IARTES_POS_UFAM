91dbf04aac2f869fd961fb1f32b5b4fa56f1f864
lucaslsg-prog <lsg.comunicacoes@gmail.com>
2022-09-25 15:43:25 -0300

    Add algoritmo to validate cpf

--- Avaliacao1/testCases.py
+++ Avaliacao1/testCases.py
@@ -2,19 +2,59 @@ import pytest
 import validation
 
 class TestEmail(object):
-
+    
     def test_email_sem_arroba(self):
-        email = 'username.gmail.com'
+        email = 'usernamegmail.com'
+        assert validation.check_email(email) == False
+
+    def test_email_com_letra_maiuscula(self):
+        email = 'USERNAME@gmail.com'
+        assert validation.check_email(email) == False
+
+    def test_email_sem_ponto_com(self):
+        email = 'username@gmail'
+        assert validation.check_email(email) == False
+
+    def test_email_sem_dominio_valido_apos_arroba(self):
+        email = 'username@qualquercoisa.com'
         assert validation.check_email(email) == False
 
     def test_email_com_espaco(self):
         email = 'username @gmail.com'
         assert validation.check_email(email) == False
     
-    def test_email_sem_ponto_com(self):
-        email = 'username@gmail'
-        assert validation.check_email(email) == False
-
     def test_email_valido(self):
-            email = 'username@gmail.com'
-            assert validation.check_email(email) == True
+        email = 'username@gmail.com'
+        assert validation.check_email(email) == True
+
+
+
+class TestCPF(object):
+    
+    def test_cpf_com_letras(self):
+        cpf = 'A27.0b5.4W2-2c'
+        assert validation.check_cpf(cpf) == False
+    
+    def test_cpf_sem_ponto(self):
+        cpf = '529982247-25'
+        assert validation.check_cpf(cpf) == False
+
+    def test_cpf_sem_hifen(self):
+        cpf = '529.982.24725'
+        assert validation.check_cpf(cpf) == False
+    
+    def test_cpf_mais_digitos_numericos(self):
+        cpf = '5291982124712516567687889'
+        assert validation.check_cpf(cpf) == False
+
+    def test_cpf_primeiro_digito(self):
+        cpf = '529.982.247-35'
+        assert validation.check_cpf(cpf) == False
+    
+    def test_cpf_segundo_digito(self):
+        cpf = '529.982.247-29'
+        assert validation.check_cpf(cpf) == False
+
+    def test_cpf_valido(self):
+        cpf = '529.982.247-25'
+        assert validation.check_cpf(cpf) == True
--- Avaliacao1/validation.py
+++ Avaliacao1/validation.py
@@ -1,25 +1,96 @@
 # algoritmo para validar email e cpf
 
-# 1) casos de teste voltados para o que não deve ter em um email
-# 2) casos de teste voltados para posições erradas ex: dominio@username
-
-
-# email = input("Digite um email: ")
 def check_email(email):
+    dominios_email = ('gmail','hotmail','yahoo','icloud','outlook')
     email_valido = True
     if '@' not in email:
         email_valido = False
-        print("Email invalido")
+        print("Email invalido, insira o '@'")
 
-    elif ' ' in email:
+    elif email.islower() != True:
         email_valido = False
-        print("Email invalido")
+        print("Email invalido, insira apenas letras minusculas")
 
     elif '.com' not in email:
         email_valido = False
-        print("Email invalido")
+        print("Email invalido, insira '.com'")
+    
+    elif (email.split("@",1)[1]).split('.com')[0] not in dominios_email:
+        email_valido = False
+        print("Email invalido, inisira um domínio valido após '@'")
+
+    elif ' ' in email:
+        email_valido = False
+        print("Email invalido, insira email sem espaço")
+
     else:
         email_valido = True
     
     return email_valido
-# check_email(email)
+
+import string
+
+def check_cpf(cpf):
+    cpf_valido = True
+    minusculas = list(string.ascii_lowercase)
+    maiusculas = list(string.ascii_uppercase)
+    letras = minusculas+maiusculas
+    for i in range(len(letras)):
+        if letras[i] not in cpf:
+            pass
+        else:
+            cpf_valido = False
+            print("CPF invalido")
+
+    if '.' in cpf:
+        cpf_valido = False
+        print("CPF invalido")
+
+    elif '-' in cpf:
+        cpf_valido = False
+        print("CPF invalido")
+
+    if len(cpf) == 14:
+        
+        primeiraParte = cpf.split('.')[0]
+        segundaParte = cpf.split('.')[1]
+        terceiraParte = (cpf.split('.')[2]).split('-')[0]
+        quartaParte = (cpf.split('.')[2]).split('-')[1]
+        digitos_string = primeiraParte + segundaParte + terceiraParte + quartaParte
+        
+        list_soma1 = []
+        list_soma2 = []
+        if len(digitos_string) == 11 and digitos_string.isdigit():
+
+            multiplicador=10
+            for digito in range(0,9):
+                list_soma1.append(multiplicador*int(digitos_string[digito]))
+                multiplicador-=1
+
+            if (sum(list_soma1)*10)%11 == int((cpf.split('.')[2]).split('-')[1][0]):
+
+                multiplicador=11  
+                for digito in range(0,10):
+                    list_soma2.append(multiplicador*int(digitos_string[digito]))
+                    multiplicador-=1
+
+                if (sum(list_soma2)*10)%11 == int((cpf.split('.')[2]).split('-')[1][1]):
+                    cpf_valido = True
+                    
+                else:
+                    cpf_valido = False
+                    print("CPF invalido")
+            else:
+                cpf_valido = False
+                print("CPF invalido")
+
+        else:
+            cpf_valido = False
+            print("CPF invalido")
+
+    else:
+        cpf_valido = False
+        print("CPF invalido")
+
+
+    return cpf_valido
